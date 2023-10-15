5bb5ab6d6280f3b752c87d14bbc1ad1fbccf5bc0
Brexton Ho <e0564446@u.nus.edu>
2023-10-15 17:46:33 +0800

    added icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import localFont from 'next/font/local'
 import type { Metadata } from 'next'
 import { poppins, drukwidebold, ltunivers } from './fonts'
 import Head from 'next/head';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 
+config.autoAddCss = false;
 
 export default function RootLayout({
   children,
@@ -18,6 +21,7 @@ export default function RootLayout({
       <Head>
         <title>BREXTONHO</title>
         <meta name='description' content='brexton ho' />
+        <link rel="icon" href="@/app/favicon.ico" />
       </Head>
       <main>{children}</main>
     </div>
